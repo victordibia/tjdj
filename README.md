07e6bf561bd533aafda9b81bbb73a1eb17eab9d9
Victor <dibiavc@us.ibm.com>
2017-01-18 12:29:15 -0500

    update spotify querry

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # tjdj
 Fun speech-based app to get TJBot playing (and dancing to) song snippets using Watson Apis and the Spotify apis
+
+
+Dependencies List
+
+- Watson Developer Cloud : [Watson Speech to Text](https://www.ibm.com/watson/developercloud/speech-to-text.html), [Watson Conversation](https://www.ibm.com/watson/developercloud/conversation.html), and [Watson Text to Speech](https://www.ibm.com/watson/developercloud/text-to-speech.html).
+- [mic](https://www.npmjs.com/package/mic) npm package : for reading audio input
+- [pigio](https://www.npmjs.com/package/pigpio) npm package : Fast (software) GPIO, PWM, servo control, state change notification, and interrupt handling on the Raspberry Pi.
+- [web-audio-api](https://www.npmjs.com/package/web-audio-api) : implementation (partial) of the HTML5 web audio api, used to decode sound files.
+- [underscorejs](https://www.npmjs.com/package/underscore) : functional programming helper library for data manipulation.
+- [node-aplay](https://www.npmjs.com/package/node-aplay) : Simple nodejs wrapper for aplay.
+- [rpi-ws281x-native] (https://www.npmjs.com/package/rpi-ws281x-native): basic set of functions to write data to a strip of ws2811/ws2812 LEDs.
--- tjdj.js
+++ tjdj.js
@@ -1,4 +1,5 @@
 var config = require('./config');  // gets our username and passwords from the config.js files
+var watson = require('watson-developer-cloud');
 var speech_to_text = watson.speech_to_text({
   username: config.STTUsername,
   password: config.STTPassword,
@@ -21,11 +22,70 @@ var _ = require('underscore');
 var request = require("request") ;
 
 
-var searchterm = "tania brown" ;
-var spotifysearchurl = "" ;
-
-request('http://www.google.com', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage.
+var searchterm = "johnny yemi" ;
+var searchtype = "track"
+var options = {
+  method: 'GET',
+  url : "https://api.spotify.com/v1/search",
+  qs : {
+    q:searchterm.replace(/ /g,"+"),
+    type : searchtype,
+    market :"US",
+    limit : 20
   }
-})
+}
+
+getSpotify();
+
+function getSpotify(){
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var result = JSON.parse(body)
+      if(result.tracks.items.length > 0) {
+        downloadFile(result.tracks.items[0].preview_url) ; // download preview file
+      }
+      result.tracks.items.forEach(function(track){
+
+        var trackartists = ""
+        track.artists.forEach(function(artist){
+          trackartists = trackartists + artist.name + ", "
+        })
+        console.log(track.name, " by " ,trackartists, track.popularity)
+      })
+    }else{
+      console.log(error + " error" + response.statusCode)
+    }
+  })
+}
+
+
+function downloadFile(url){
+  var dest = "musci.wav"
+  var file = fs.createWriteStream(dest);
+  var donwloadrequest = request.get(url);
+
+    // verify response code
+    donwloadrequest.on('response', function(response) {
+        if (response.statusCode !== 200) {
+            return cb('Response status was ' + response.statusCode);
+        }
+    });
+
+    // check for request errors
+    donwloadrequest.on('error', function (err) {
+        fs.unlink(dest);
+    });
+    donwloadrequest.pipe(file);
+    file.on('finish', function() {
+        file.close();
+        playSound();
+    });
+
+    file.on('error', function(err) { // Handle errors
+        fs.unlink(dest); // Delete the file async. (But we don't check the result)
+    });
+}
+
+function playSound(){
+
+}
