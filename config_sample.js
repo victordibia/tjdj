58c567b4d0b392891987486cb2fa8f24ce21055b
Victor <dibiavc@us.ibm.com>
2017-01-18 09:41:21 -0500

    first commit

---
+++ config_sample.js
@@ -0,0 +1,22 @@
+// Please replace the username and password with your bluemix credentials
+
+
+exports.STTpassword = 'xxx-xxx-xxx' ;   // Speech to text password
+exports.STTusername = 'xxx-xxx-xxx' ;    // Speech to text username
+exports.STTCustomizationid = 'xxxxxxxxx'   // Speech to text Customization id (if you are using a customized language model)
+
+//Credentials for Watson Text to Speech service
+
+exports.TTSPassword = 'xxx-xxx-xxx' ;   // Text to speech password
+exports.TTSUsername = 'xxx-xxx-xxx' ;
+
+// Credentials for Watson Conversation service
+exports.ConPassword = 'xxxxxx' ;         // conversation service password
+exports.ConUsername = 'xxx-xxx-xxx' ;   // conversation service username
+exports.ConWorkspace = 'xxx-xxx-xxx';     // Conversation workspace id
+
+// Credentials for Vision Api
+exports.VisionKey = 'xxx-xxx-xxx' ;
+exports.VisionVersion = '2016-05-19' ;
+
+exports.version = 'v1' ;
